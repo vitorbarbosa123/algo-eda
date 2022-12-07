2ab533bc42a2064652028e6dd437504a5d127391
80813598/Vitor Barbosa <jose.vitor.b.maciel@accenture.com>
2022-12-07 13:41:56 -0300

    [ADD] height

--- AULAS/arvoresBinarias/BST.java
+++ AULAS/arvoresBinarias/BST.java
@@ -150,4 +150,13 @@ public class BST {
       return aux;
     }
   }
+
+  public int height() {
+    return height(this.root);
+  }
+
+  private int height(Node node) {
+    if(node == null) return -1;
+    else return 1 + Math.max(height(node.left), height(node.right));
+  }
 }
